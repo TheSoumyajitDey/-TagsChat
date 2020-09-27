30fb045000a40a30a6ae366561e84c7b69852ede
Soumyajit Dey <55289316+TheSoumyajitDey@users.noreply.github.com>
2020-09-27 20:28:55 +0530

    UI added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatbarComponent } from './chatbar/chatbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chatbar/chatbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-chatbar",
+  templateUrl: "./chatbar.component.html",
+  styleUrls: ["./chatbar.component.scss"],
+})
+export class ChatbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  
+}
