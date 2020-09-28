7e50c148bfd60d713aa3143c50bbb49191e316ac
Soumyajit Dey <55289316+TheSoumyajitDey@users.noreply.github.com>
2020-09-28 18:23:27 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChatbarComponent } from './chatbar/chatbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ChatbarComponent } from "./chatbar/chatbar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ChatbarComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/chatbar/chatbar.component.ts
+++ src/app/chatbar/chatbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-chatbar",
@@ -6,9 +7,33 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./chatbar.component.scss"],
 })
 export class ChatbarComponent implements OnInit {
+  chat: string;
+  chats: any[] = [];
+  items: string[] = ["Noah", "Liam", "Mason", "Jacob"];
+  values = "";
+  isVisible = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isVisible = false;
+  }
 
-  
+  onChatAdded() {
+    this.chats.push(this.chat);
+    this.chat = "";
+  }
+
+  onKey(event: KeyboardEvent) {
+    if (
+      (event.target as HTMLInputElement).value.charAt(
+        (event.target as HTMLInputElement).value.length - 1
+      ) == "@"
+    ) {
+      console.log("@ working ");
+      this.isVisible = true;
+    } else {
+      this.isVisible = false;
+    }
+  }
 }
