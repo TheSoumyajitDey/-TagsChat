2468c87bdd10cebef2a57a36817b678c93f75df6
Soumyajit Dey <55289316+TheSoumyajitDey@users.noreply.github.com>
2020-10-02 20:11:34 +0530

    control view accessor

--- src/app/editablr/editablr.component.ts
+++ src/app/editablr/editablr.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ControlValueAccessor } from "@angular/forms";
 
 @Component({
-  selector: 'app-editablr',
-  templateUrl: './editablr.component.html',
-  styleUrls: ['./editablr.component.scss']
+  selector: "app-editablr",
+  templateUrl: "./editablr.component.html",
+  styleUrls: ["./editablr.component.scss"],
 })
-export class EditablrComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class EditablrComponent implements OnInit, ControlValueAccessor {
+  constructor() {}
+  writeValue(obj: any): void {
+    throw new Error("Method not implemented.");
+  }
+  registerOnChange(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
   }
 
+  ngOnInit(): void {}
 }
